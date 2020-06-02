4623e1f23cd7a8ed3e7bda5d4131bb80c1b00b72
Genesis <genesisrhapsodos98@gmail.com>
2020-06-02 13:18:19 +0700

    Reformatted test files

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
 const app = require('../../src/app');
 
-describe('\'users\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('users');
+// eslint-disable-next-line quotes
+describe("'users' service", () => {
+	it('registered the service', () => {
+		const service = app.service('users');
 
-    assert.ok(service, 'Registered the service');
-  });
+		assert.ok(service, 'Registered the service');
+	});
 });

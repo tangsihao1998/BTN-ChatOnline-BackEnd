490086848d730803dc0fb79f893c192e5f828468
Genesis <genesisrhapsodos98@gmail.com>
2020-06-02 13:00:10 +0700

    Configured ESLint to use tabs for indentation. Reformatted all files.

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 const logger = require('./logger');
 
-module.exports = function (app) {
-  mongoose.connect(
-    app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
-  ).catch(err => {
-    logger.error(err);
-    process.exit(1);
-  });
-  
-  mongoose.Promise = global.Promise;
+module.exports = function(app) {
+	mongoose
+		.connect(app.get('mongodb'), { useCreateIndex: true, useNewUrlParser: true })
+		.catch((err) => {
+			logger.error(err);
+			process.exit(1);
+		});
 
-  app.set('mongooseClient', mongoose);
+	mongoose.Promise = global.Promise;
+
+	app.set('mongooseClient', mongoose);
 };
